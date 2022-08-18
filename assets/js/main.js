f0ed68cc87f053d609c0b685e94caefced2dd06e
hacene <38687021+hacene1986@users.noreply.github.com>
2022-08-18 17:14:26 +0200

    Add files via upload
    swiper

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,7 +90,18 @@ const modalViews = document.querySelectorAll('.services__modal'),
       })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
